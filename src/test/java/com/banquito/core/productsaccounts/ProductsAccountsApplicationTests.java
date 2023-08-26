f60894037364f60c03cd57ffed217339fc29e098
Christopher <cramos@plugthem.social>
2023-08-25 21:27:51 -0500

    First Commit

---
+++ src/test/java/com/banquito/core/productsaccounts/ProductsAccountsApplicationTests.java
@@ -0,0 +1,15 @@
+// package com.banquito.core.productsaccounts;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class ProductsAccountsApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+
+
+// }
